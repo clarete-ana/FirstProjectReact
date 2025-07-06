ff127de9d8299bba52b442f47d81c7504815fc0e
Clarete-ana <anaclaraclarete2@gmail.com>
2025-07-06 15:24:47 -0300

    Criando containers com styled-components

--- src/componentes/OpcoesHeader/index.js
+++ src/componentes/OpcoesHeader/index.js
@@ -1,15 +1,29 @@
-import './estilo.css';
+import styled from 'styled-components';
 
+const Opcao = styled.li`
+  display: flex;
+`
+const Opcoes = styled.ul`
+  display: flex;
+  font-size: 16px;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  min-width: 120px;
+  height: 100%;
+  padding: 0 5px;
+  cursor: pointer;
+`
 const textoOpcoes = ['CATEGORIAS' , 'FAVORITOS' , 'MINHA ESTANTE'];
 
 function OpcoesHeader()
 {
   return(
-    <ul className='opcoes'>
+    <Opcoes>
           {textoOpcoes.map( (texto) => (
-            <li className='opcao'><p>{texto}</p></li>
+            <Opcao><p>{texto}</p></Opcao>
             ) ) }
-        </ul>
+        </Opcoes>
   )
 }
 
